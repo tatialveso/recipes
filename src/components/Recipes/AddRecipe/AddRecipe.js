a6928a57529144ed9863548a70b8fe8532015832
tatialveso <tatiialveso@gmail.com>
2022-11-11 17:52:08 -0300

    :sparkles: style form

--- src/components/Recipes/AddRecipe/AddRecipe.js
+++ src/components/Recipes/AddRecipe/AddRecipe.js
@@ -1,7 +1,8 @@
 import { useState } from "react"
-import { Button, Form } from "react-bootstrap"
+import { Button, Form, Modal } from "react-bootstrap"
 
 function AddRecipe({ recipes, setRecipes }) {
+    const [show, setShow] = useState(false)
     const [form, setForm] = useState({
         name: "",
         image: "",
@@ -9,6 +10,9 @@ function AddRecipe({ recipes, setRecipes }) {
         level: "",
         preparation: ""
     })
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
@@ -26,65 +30,81 @@ function AddRecipe({ recipes, setRecipes }) {
             level: "",
             preparation: ""
         })
+
+        setShow(false)
     }
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3">
-                <Form.Label>Nome da receita</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Insira o nome da receita"
-                    name="name"
-                    onChange={handleChange}
-                    value={form.name}
-                />
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Imagem da receita pronta</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Insira o endereço da imagem da receita"
-                    name="image"
-                    onChange={handleChange}
-                    value={form.image}
-                />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-                <Form.Label>Quantidade de porções</Form.Label>
-                <Form.Control
-                    type="number"
-                    placeholder="Insira o número de porções da receita"
-                    name="servings"
-                    onChange={handleChange}
-                    value={form.servings}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Nível de Dificuldade</Form.Label>
-                <Form.Select name="level" onChange={handleChange}>
-                    <option value="0">Selecione uma opção</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                </Form.Select>
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Preparação da receita</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Insira o modo de preparo da receita"
-                    name="preparation"
-                    onChange={handleChange}
-                    value={form.preparation}
-                />
-            </Form.Group>
-            <Button className="mb-3" variant="primary" type="submit">
-                Cadastrar
+        <div>
+            <Button className="my-3" variant="secondary" onClick={handleShow}>
+                Cadastrar uma nova receita
             </Button>
-        </Form>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Cadastrar receita</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Nome da receita</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Insira o nome da receita"
+                                name="name"
+                                onChange={handleChange}
+                                value={form.name}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Imagem da receita pronta</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Insira o endereço da imagem da receita"
+                                name="image"
+                                onChange={handleChange}
+                                value={form.image}
+                            />
+                        </Form.Group>
+                        <div className="d-flex justify-content-between">
+                            <Form.Group className="mb-3">
+                                <Form.Label>Quantidade de porções</Form.Label>
+                                <Form.Control
+                                    type="number"
+                                    placeholder="Insira o número de porções da receita"
+                                    name="servings"
+                                    onChange={handleChange}
+                                    value={form.servings}
+                                />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Nível de Dificuldade</Form.Label>
+                                <Form.Select name="level" onChange={handleChange}>
+                                    <option value="0">Selecione uma opção</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </div>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Preparação da receita</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Insira o modo de preparo da receita"
+                                name="preparation"
+                                onChange={handleChange}
+                                value={form.preparation}
+                            />
+                        </Form.Group>
+                        <Button className="mb-3" variant="primary" type="submit">
+                            Cadastrar
+                        </Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+        </div>
     )
 }
 
