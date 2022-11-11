b4633d03e1831d14353181967553d92235d16f73
tatialveso <tatiialveso@gmail.com>
2022-11-11 17:15:35 -0300

    :sparkles: create about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,12 @@
+import { Container } from "react-bootstrap"
+
 function About() {
     return (
-        <h1>Sobre o site</h1>
+        <Container>
+            <h1 className="my-3">Sobre o site</h1>
+            <p>Este projeto tem como objetivo treinar o processo de CRUD com o React.</p>
+            <p>Navegue aplicação pelo menu.</p>
+        </Container>
     )
 }
 
