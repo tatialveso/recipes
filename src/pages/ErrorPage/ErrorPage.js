2b7303ae192bf61df26463525aeb5ef474742acb
tatialveso <tatiialveso@gmail.com>
2022-11-11 17:25:32 -0300

    :sparkles: create error page

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,6 +1,16 @@
+import { Alert, Container } from "react-bootstrap"
+
+
 function ErrorPage() {
     return (
-        <h1>404</h1>
+        <Container>
+            <Alert className="my-5" variant="danger">
+                <Alert.Heading>Ops! Você tentou entrar em um lugar que não existe 😞</Alert.Heading>
+                <p>
+                    Volte para o menu e navegue pelas rotas autorizadas. Qualquer coisa, tente novamente mais tarde.
+                </p>
+            </Alert>
+        </Container>
     )
 }
 
